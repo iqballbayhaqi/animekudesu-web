b9a2493383ba9058c3e1f3258b7eec3056382eb9
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2025-05-04 17:18:49 +0700

    fix: update dataSupport to replace Sword Art Online entry with A Rank Party details

--- src/utils/dataSupport.ts
+++ src/utils/dataSupport.ts
@@ -10,9 +10,9 @@ const dataSupport = [
     landscape: 'https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=cover,format=auto,quality=85,width=1920/keyart/GEXH3W29Z-backdrop_wide'
   },
   {
-    slug: 'sword-art-online',
-    logo: 'https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=480/CurationAssets/SwordArtOnline/SEASON%205/ULTRA-WIDE/SwordArtOnline-S5-KV1-UW-Logo.png',
-    landscape: 'https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=cover,format=auto,quality=85,width=1920/keyart/GXJHM3GX8-backdrop_wide'
+    slug: 'a-rank-party-wo-ridatsu-shita-ore-wa-moto-oshiego-tachi-to-meikyuu-shinbu-wo-mezasu',
+    logo: 'https://arank-party-ridatsu-official.com/assets/img/common/logo.png',
+    landscape: 'https://a.storyblok.com/f/178900/1320x741/33b72e8840/i-left-my-a-rank-party-to-help-my-students-reach-the-dungeon-depths.jpg/m/1200x0/filters:quality(95)format(webp)'
   }
 ]
 
